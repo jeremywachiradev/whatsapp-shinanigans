a9e84e2c59ef53bf2e793e7bedbc9b9914008b51
jrme <jeremywachiradev@gmail.com>
2025-11-25 14:58:14 +0300

    fix prisma

--- next.config.js
+++ next.config.js
@@ -3,11 +3,18 @@
  * for Docker builds.
  */
 import "./src/env.js";
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 /** @type {import("next").NextConfig} */
 const config = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
   },
   async headers() {
     return [
